658d3b4541a56ef35c65f658b131a3bb01e4f9db
Michal Cholajczyk <michalcholajczyk@gmail.com>
2023-03-13 12:41:19 +0100

    background images

---
+++ client/src/Layout/Layout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Header from "../components/Header/Header";
+import Home from "../components/Home/Home";
+import Expertise from "../components/Expertise/Expertise";
+import Work from "../components/Work/Work";
+import Experience from "../components/Experience/Experience.jsx";
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
+
+
+
+const Layout = () => {
+	return (
+		<>
+			<Header />
+			<Home/>
+			<Expertise/>
+			<Work/>
+			<Experience/>
+			<Contact/>
+			<Footer/>
+
+		</>
+	);
+};
+
+export default Layout;
